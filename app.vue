640ff5c69f72755c5419d06bb5962f5b7a0a8e19
zhongpei <1000@intry.cn>
2024-04-20 10:29:59 +0800

    init

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <h2>appConfig.theme :{{ appConfig.theme }}</h2>
+    <!-- <AppAlert> This is an auto-imported component. </AppAlert> -->
+
+    <NuxtWelcome />
+  </div>
+</template>
+
+
+<script setup lang="ts">
+const appConfig = useAppConfig()
+
+console.log(appConfig.theme)
+</script>
+
